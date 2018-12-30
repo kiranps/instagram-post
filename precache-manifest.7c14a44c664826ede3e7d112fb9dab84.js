7523ab1cfdf66c3914fc5bac5d4d94fc59027d77
Kiran PS <pskirann@gmail.com>
2018-12-30 20:01:56 +0530

    Updates

---
+++ precache-manifest.7c14a44c664826ede3e7d112fb9dab84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ba30fbd9ec19ce97b6",
+    "url": "/instagram-post/static/css/main.eed310e4.chunk.css"
+  },
+  {
+    "revision": "82ba30fbd9ec19ce97b6",
+    "url": "/instagram-post/static/js/main.82ba30fb.chunk.js"
+  },
+  {
+    "revision": "6e59e04694fdb1356588",
+    "url": "/instagram-post/static/js/1.6e59e046.chunk.js"
+  },
+  {
+    "revision": "a1c1269346b7ac945807",
+    "url": "/instagram-post/static/js/runtime~main.a1c12693.js"
+  },
+  {
+    "revision": "6a3098f971646fda92757c49be088e27",
+    "url": "/instagram-post/index.html"
+  }
+];
